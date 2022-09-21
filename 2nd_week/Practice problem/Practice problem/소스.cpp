825b91376b9db2aa07294794fb280febe219d4a9
gaso2580 <choiyoungho2000@naver.com>
2022-09-21 14:59:44 +0900

    2nd week
    1st problem

--- 2nd_week/Practice problem/Practice problem/소스.cpp
+++ 2nd_week/Practice problem/Practice problem/소스.cpp
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-	printf("학번 : %d\n", 2020010535);
-	printf("이름 : %s\n", "Kim_Geon_Hui");
-	printf("학점 : %c\n", 'A');
+	printf("class of : %d\n", 2020010535);
+	printf("name : %s\n", "Kim_Geon_Hui");
+	printf("grade : %c\n", 'A');
 
 	return 0;
 
-}
+}	
