d32f758fc5d2d69a05806d64f9fdde727378c35c
GEONI <rlarjsgml03@naver.com>
2022-09-21 15:38:58 +0900

    2nd week
    1st

--- 2nd_week/Practice problem/Practice problem/소스.cpp
+++ 2nd_week/Practice problem/Practice problem/소스.cpp
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	printf("class of : %d\n", 2020010535);
-	printf("name : %s\n", "Kim_Geon_Hui");
-	printf("grade : %c\n", 'A');
+	printf("Class of : %d\n", 2020010535);
+	printf("Name : %s\n", "Kim_Geon_Hui");
+	printf("Grade : %c\n", 'A');
 
 	return 0;
 
