c04bcc8124ebcce16b9f1d593d69c7d8e3f5ec81
gaso2580 <choiyoungho2000@naver.com>
2022-09-21 14:56:48 +0900

    1st week
    Hello World!

---
+++ 2nd_week/2nd/2nd/소스.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int Korean=70, English=80, Math=90;
+	int tot;
+
+	tot = Korean + English + Math;
+	printf("Korean=%d, English=%d, Math=%d\n"), Korean, English, Math;
+	printf("%d\n"), tot;
+
+	return 0;
+
+}
---
+++ 2nd_week/Practice problem/Practice problem/소스.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("학번 : %d\n", 2020010535);
+	printf("이름 : %s\n", "Kim_Geon_Hui");
+	printf("학점 : %c\n", 'A');
+
+	return 0;
+
+}
